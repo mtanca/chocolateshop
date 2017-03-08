6c3e26123c0bcc4e3ce948a9220b7a37e1ad0902
MARK <m.tanca@hotmail.com>
2017-03-07 19:48:53 -0500

    changes

---
+++ spec/format_spec.rb
@@ -0,0 +1,10 @@
+describe Format do
+  context '.to_s' do
+    let (:order) { { 'milk': 7, 'dark': 0, 'white': 2, 'sugar free': 2 } }
+    let (:correct_string) { "milk 7, dark 0, white 2, sugar free 2 \n" }
+
+    it 'should format data as a string' do
+      expect(Format.new(order).to_s).to match correct_string
+    end
+  end
+end
