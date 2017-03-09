907eb74230aaabe28b1dbc27b96109635c16af42
MARK <m.tanca@hotmail.com>
2017-03-08 22:08:06 -0500

    yes

--- spec/order_file_writer_spec.rb
+++ spec/order_file_writer_spec.rb
@@ -3,7 +3,7 @@ describe OrderFileWriter do
     let(:path) { Tempfile.new('test_redemptions.cvs').path }
     let(:order_batch) { "order1 1 \n order2 2 \n order3 3" }
 
-    it 'should write order batch to file via path given' do
+    it 'should write an order batch to disk via path given' do
       OrderFileWriter.new(path).write(order_batch)
       expect(File.open(path).read).to match order_batch
     end
